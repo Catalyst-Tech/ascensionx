906526768deb40845fa928b41151fb6492f21ab2
NidalGhonaim <nidalghonaim@gmail.com>
2022-06-10 14:36:25 +0100

    fix home button url

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,7 +3,7 @@ import Button from "@mui/material/Button";
 const Menu: React.FC = () => {
   return (
     <nav className="menu">
-      <Button href="/ascensionx">Home</Button>
+      <Button href="/">Home</Button>
       {/*<Button href="/ascensionx/buy">Buy $ASCN</Button>*/}
       <Button href="#/ascensionx/whitepaper">Whitepaper</Button>
       {/*<Button href="/ascensionx/gettingStarted">Getting Started</Button>*/}
